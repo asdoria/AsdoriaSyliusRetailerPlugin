eafe7ccbcbbba61241cd90a2ef37ad012de98623
Benjamin Da Cruz <benjamin.dacruz@asdoria.com>
2022-05-30 11:33:58 +0200

    Affichage de la carte non-réglée fonctionnel, récupération des données fonctionnelle

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 const basePath = path.resolve(__dirname, './');
 const assets_path = path.join(basePath, './src/Resources/private');
 const output_path = path.join(basePath, './src/Resources/public');
-const public_path = 'bundles/asdoriaretailer';
-const sass_path = path.join(assets_path, './sass');
+const public_path = 'bundles/asdoriasyliusretailerplugin';
+const sass_path = path.join(assets_path, './scss');
 const js_path = path.join(assets_path, './js');
 const isProduction = Encore.isProduction();
 
@@ -22,6 +22,12 @@ Encore
 
   .addEntry('admin-retailer', [
     path.join(js_path, './admin.js'),
+    path.join(sass_path, './admin.scss'),
+  ])
+
+  .addEntry('shop-retailer', [
+    path.join(js_path, './shop.js'),
+    path.join(sass_path, './shop.scss'),
   ])
 
   // allow sass/scss files to be processed
@@ -29,18 +35,20 @@ Encore
   .enablePostCssLoader()
 
   // allow legacy applications to use $/jQuery as a global variable
-  .autoProvidejQuery()
+  // .autoProvidejQuery()
 
   .enableSourceMaps(!isProduction)
 
   .disableSingleRuntimeChunk()
 
   // create hashed filenames (e.g. app.abc123.css)
-  .enableVersioning(isProduction)
+  .enableVersioning(false)
   .configureFilenames({
     js: '[name].min.js',
     css: '[name].min.css',
   })
+
+  .enableVueLoader();
 ;
 
 config = Encore.getWebpackConfig();
